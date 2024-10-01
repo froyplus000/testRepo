01ee25b00614158a5e82bb4e8dd1cf7b28264d45
Pattarapol Tantechasa <froyplus@hotmail.co.th>
2024-10-01 12:58:00 +1000

    Setup Project Structure

--- react-basic/src/App.jsx
+++ react-basic/src/App.jsx
@@ -1,9 +1,9 @@
-import Navbar from "./components/Navbar.jsx";
+import Navbar from "./components/shared/Navbar.jsx";
 import { Route, Routes } from "react-router";
-import Home from "./components/Home.jsx";
-import About from "./components/About.jsx";
-import Contact from "./components/Contact.jsx";
-import Products from "./components/Products.jsx";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import Products from "./components/Products/Products.jsx";
 
 function App() {
   return (
